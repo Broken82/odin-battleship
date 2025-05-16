890f3e45d42c2b0655f9cd3bf100d625fdaf837a
Broken82 <zgrass11@gmail.com>
2025-05-17 00:58:15 +0200

    add gameboard methods

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,11 +1,46 @@
 
 export function Gameboard(){
-    let gameboard = Array.from(Array(8), () => new Array(8))
+    let board = Array.from(Array(10), () => new Array(10))
 
     function place(x, y, ship, orientation){
         let length = ship.length
+
+        if(!inbound(x,y,ship,orientation)){
+            throw new Error('Coordinates out of bounds!')
+        }
+
+        if(orientation == 'vertical'){
+            for(let i = x; i < x + length; i++){
+                this.board[i][y] = ship
+            }
+        }
+        else{
+            for(let i = y; i < y + length; i++){
+                this.board[x][i] = ship
+            }
+        }
         
     }
 
+    function inbound(x, y, ship, orientation){
+
+        let length = ship.length
+
+        if(x < 0 || y < 0){
+            return false
+        }
+
+        if(orientation == 'vertical' &&  x + length - 1 > board.length - 1) return false;
+        if(orientation == 'horizontal' && y + length - 1 > board.length - 1) return false;
+
+        return true
+    }
+
+    function receiveAttack(x, y){
+        
+    }
+
+
+    return {board, place}
 
 }
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,2 +1,25 @@
 import { Gameboard } from "../gameboard";
+import { Ship } from "../ship";
 
+test('places a ship on board', () => {
+    let ship = Ship(2)
+    let board = Gameboard()
+
+    board.place(0,0,ship,'vertical')
+
+
+    expect(board.board[0][0]).toBe(ship)
+    
+})
+
+test('cant place ships out of bounds', () =>{
+    let ship = Ship(2)
+    let board = Gameboard()
+
+    
+
+    expect(() => {
+        board.place(10,10,ship,'vertical')
+    }
+    ).toThrow(Error)
+})
